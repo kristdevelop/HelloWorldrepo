e239ecfadc2d32ac659d424c8b239c75ebad7971
kristdevelop <kyllirist@gmail.com>
2017-01-17 12:37:21 +0200

    Add files via upload

---
+++ src/helloworldToDelete.java
@@ -0,0 +1,16 @@
+package com.example.helloworld;
+
+import com.sun.tools.javac.jvm.ClassWriter;
+
+/**
+ * Created by kylli on 10/5/16.
+ */
+
+//see on kõigest koodijupp, aga võttis alles tegu, aega ja mitu repositooriumi loomist, et need local arvutist üles saada
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hey You");
+
+    }
+}
